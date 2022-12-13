918bae1a612959fe1917f68073d434e43d5d1b6c
Yor-Jihons <ryo.malon814@gmail.com>
2022-12-13 11:28:11 +0900

    Created the template.html.

---
+++ main/src/main.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace BookmarkCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#!");
+            for( int i = 0; i < 3; i++ ){
+                Console.WriteLine( "C# " + i );
+            }
+        }
+    }
+}
