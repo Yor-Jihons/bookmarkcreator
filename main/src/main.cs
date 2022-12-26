9153efbf3683ffdfe914157ce52edea9edde961d
Yor-Jihons <ryo.malon814@gmail.com>
2022-12-26 19:53:06 +0900

    Added the comments.

--- main/src/main.cs
+++ main/src/main.cs
@@ -8,15 +8,20 @@ namespace BookmarkCreator
         {
             try
             {
+                // Analize the command-line arguments.
                 var cmdline = CmdLines.CommandLine.Create( args );
                 if( cmdline == null ) return;
 
+                // Read the definition file.
                 var tags = Executions.Executor.Read( cmdline.DefinitionFilePath );
 
+                // Make the object of dictionary data.
                 string contentString = Executions.Executor.CreateContentString( tags );
 
+                // Make the result html string.
                 string htmlString = Executions.Executor.CreateHtmlString( cmdline.TemplateFilePath, contentString );
 
+                // Write the file.
                 (new Factories.FilePrinter( cmdline.OutputFilePath )).Print( htmlString );
 
                 Console.WriteLine( "Created " + cmdline.OutputFilePath + " the bookmark-html file." );
