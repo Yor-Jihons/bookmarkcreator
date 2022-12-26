88604698e329fee5013f69d382657a30efa88935
Yor-Jihons <ryo.malon814@gmail.com>
2022-12-26 18:28:19 +0900

    Separated the processes.

--- main/src/main.cs
+++ main/src/main.cs
@@ -13,56 +13,13 @@ namespace BookmarkCreator
                 var cmdline = CmdLines.CommandLine.Create( args );
                 if( cmdline == null ) return;
 
-                //Console.WriteLine( cmdline.DefinitionFilePath );
-                //Console.WriteLine( cmdline.OutputFilePath );
-                //Console.WriteLine( cmdline.Title );
+                var tags = Executions.Executor.Read( cmdline.DefinitionFilePath );
 
-                //Console.WriteLine( cmdline.TemplateFilePath );
+                string contentString = Executions.Executor.CreateContentString( tags );
 
-                var data = Csvs.CsvReader.Read( cmdline.DefinitionFilePath );
+                string htmlString = Executions.Executor.CreateHtmlString( cmdline.TemplateFilePath, contentString );
 
-                Dictionary<string, Csvs.DataList> tags = new Dictionary<string, Csvs.DataList>();
-                foreach( var d in data )
-                {
-                    var genres = d.GetGenres();
-                    foreach( var genre in genres )
-                    {
-                        if( !tags.ContainsKey( genre ) )
-                        {
-                            tags[ genre ] = new Csvs.DataList( genre );
-                        }
-
-                        tags[ genre ].Add( d );
-                    }
-                }
-
-                var listBuilder = new StringBuilder();
-                listBuilder.Append( "    <ul>\n" );
-                var tableBuilder = new StringBuilder();
-
-                foreach( KeyValuePair<string, Csvs.DataList> item in tags )
-                {
-                    listBuilder.Append( item.Value.ToListHtmlString() );
-                    listBuilder.Append( "\n" );
-                    tableBuilder.Append( item.Value.ToTableHtmlString() );
-                    tableBuilder.Append( "\n" );
-                }
-
-                listBuilder.Append( "    </ul>\n\n" );
-
-                var contentBuilder = new StringBuilder( listBuilder.ToString() );
-                contentBuilder.Append( tableBuilder );
-
-                var template = (new Factories.TemplateFileScanner( cmdline.TemplateFilePath )).Scan();
-
-                var resultBuilder = new StringBuilder( template );
-                //resultBuilder.Replace( "[HERE]", contentBuilder.ToString() );
-
-                //resultBuilder.Replace( "[TITLE]", cmdline.Title );
-
-                //Console.WriteLine( "[RESULT]\n" + resultBuilder.ToString() );
-
-                (new Factories.FilePrinter( cmdline.OutputFilePath )).Print( resultBuilder.ToString() );
+                (new Factories.FilePrinter( cmdline.OutputFilePath )).Print( htmlString );
 
                 Console.WriteLine( "Created " + cmdline.OutputFilePath + " the bookmark-html file" );
             }
