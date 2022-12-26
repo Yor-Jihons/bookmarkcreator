7458b14b0e4b74fca14367d52d32912c73032b55
Yor-Jihons <ryo.malon814@gmail.com>
2022-12-26 19:42:07 +0900

    Added the comments.

--- main/src/Factories/FilePrinter.cs
+++ main/src/Factories/FilePrinter.cs
@@ -1,14 +1,28 @@
-using System;
+/**
+* @file
+* @brief The class to write the file.
+*/
 
 namespace BookmarkCreator.Factories
 {
+    /// <summary>
+    /// The class to write the file.
+    /// </summary>
     public class FilePrinter
     {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="filepath">The file path which the user wants to create.</param>
         public FilePrinter( string filepath )
         {
             this.FilePath = filepath;
         }
 
+        /// <summary>
+        /// Write the data to the file.
+        /// </summary>
+        /// <param name="htmlText">The data, which is a html.</param>
         public void Print( string htmlText )
         {
             using(var sr = new System.IO.StreamWriter( this.FilePath, false, new System.Text.UTF8Encoding( false ) ) )
@@ -17,6 +31,7 @@ namespace BookmarkCreator.Factories
             }
         }
 
+        /// <value>The file path which the user wants to create.</value>
         private string FilePath{ get; set; }
     }
 }
