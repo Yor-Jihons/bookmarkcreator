6673737f7dad6bca09c092b87e2464047524e827
Yor-Jihons <ryo.malon814@gmail.com>
2022-12-30 17:19:29 +0900

    Added the test for the class CommandLine.

--- main.xTests/UnitTest1.cs
+++ main.xTests/UnitTest1.cs
@@ -1,12 +1,24 @@
+using System;
 using Xunit;
 
 namespace main.xTests;
 
 public class UnitTest1
 {
+    /// <summary>
+    /// Test for the class CommandLine.
+    /// </summary>
     [Fact]
-    public void Test1()
+    public void TestCommandLine()
     {
+        string[] args1 = new string[]{ "--definition=def_file.txt", "--output=result_file.html", "--title=Bookmark1", "--template=template.html" };
+        var cmdline = BookmarkCreator.CmdLines.CommandLine.Create( args1 );
 
+        Assert.NotSame( cmdline, null );
+
+        Assert.Equal( cmdline.DefinitionFilePath, "def_file.txt" );
+        Assert.Equal( cmdline.OutputFilePath, "result_file.html" );
+        Assert.Equal( cmdline.Title, "Bookmark1" );
+        Assert.Equal( cmdline.TemplateFilePath, "template.html" );
     }
 }
