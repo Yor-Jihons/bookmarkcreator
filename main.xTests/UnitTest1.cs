340570bb3cb4b648dc977dae4e5fcae756bb9b6a
Yor-Jihons <ryo.malon814@gmail.com>
2023-02-04 19:05:32 +0900

    Added the text for the class Genre.

--- main.xTests/UnitTest1.cs
+++ main.xTests/UnitTest1.cs
@@ -9,16 +9,33 @@ public class UnitTest1
     /// Test for the class CommandLine.
     /// </summary>
     [Fact]
-    public void TestCommandLine()
+    public void CommandLineTest()
     {
         string[] args1 = new string[]{ "--definition=def_file.txt", "--output=result_file.html", "--title=Bookmark1", "--template=template.html" };
-        var cmdline = BookmarkCreator.CmdLines.CommandLine.Create( args1 );
+        var cmdline1 = BookmarkCreator.CmdLines.CommandLine.Create( args1 );
 
-        Assert.NotSame( cmdline, null );
+        Assert.NotSame( cmdline1, null );
 
-        Assert.Equal( cmdline.DefinitionFilePath, "def_file.txt" );
-        Assert.Equal( cmdline.OutputFilePath, "result_file.html" );
-        Assert.Equal( cmdline.Title, "Bookmark1" );
-        Assert.Equal( cmdline.TemplateFilePath, "template.html" );
+        Assert.Equal( cmdline1.DefinitionFilePath, "def_file.txt" );
+        Assert.Equal( cmdline1.OutputFilePath, "result_file.html" );
+        Assert.Equal( cmdline1.Title, "Bookmark1" );
+        Assert.Equal( cmdline1.TemplateFilePath, "template.html" );
+    }
+
+    [Fact]
+    public void GenreTest()
+    {
+        string text = "#English#Spanish#French";
+        var genre = BookmarkCreator.Csvs.Genre.Create( text );
+
+        Assert.Equal( " #English #Spanish #French", genre.ToString() );
+
+        Assert.Equal( "Spanish", genre.Genres[1] );
+    }
+
+    [Fact]
+    public void DataCreationTest()
+    {
+        //string text = "How2Learn,#English#Spanish#French";
     }
 }
